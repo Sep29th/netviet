7b61e839ef67fc7999d4bebe6f1025e0a632b091
Sep29th <caulata1234@gmail.com>
2025-06-06 01:06:59 +0700

    remove dto because out memory

--- src/indices/dtos/pagination-query.dto.ts
+++ src/indices/dtos/pagination-query.dto.ts
@@ -1,17 +1,19 @@
-import { Transform } from 'class-transformer';
-import { IsNumber, IsOptional, Max, Min } from 'class-validator';
+// import { Transform } from 'class-transformer';
+// import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationQueryDTO {
-  @Transform(({ value }) => parseInt(value))
-  @IsOptional()
-  @IsNumber()
-  @Min(1)
+  // @Transform(({ value }) => parseInt(value))
+  // @IsOptional()
+  // @IsNumber()
+  // @Min(1)
   page: number = 1;
 
-  @Transform(({ value }) => parseInt(value))
-  @IsOptional()
-  @IsNumber()
-  @Min(1)
-  @Max(50)
+  // @Transform(({ value }) => parseInt(value))
+  // @IsOptional()
+  // @IsNumber()
+  // @Min(1)
+  // @Max(50)
   limit: number = 10;
 }
+
+// Không dùng vì deploy bị out memory
